5185848c4d7cf24666b3a57ed37d16c432ca1400
Ajinkya <ajinkya.edu6643@gmail.com>
2026-06-05 15:46:59 +0530

    fix: Add Netlify URL to CORS configuration

--- src/main/java/com/ajinkya/skilltraining/config/SecurityConfig.java
+++ src/main/java/com/ajinkya/skilltraining/config/SecurityConfig.java
@@ -13,7 +13,9 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                		"http://localhost:3000",
+                		"https://aesthetic-mochi-524a8e.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
